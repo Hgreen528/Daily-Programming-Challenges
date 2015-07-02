c44c4736a8e18569c60e56f4f80acb15b83eb92a
Hunter Green <hgreen@spsu.edu>
2015-07-02 19:48:48 -0400

    Errors in easy challenge 221 fixed, medium Challenge 221 completed

--- Easy Challenges/Easy Challenge 221/Easy Challenge 221/Program.cs
+++ Easy Challenges/Easy Challenge 221/Easy Challenge 221/Program.cs
@@ -8,30 +8,25 @@ using System.Threading.Tasks;
 
 //Word Snake: Takes in a sequence of words in all caps where the first letter of the next word is the same
 //as the last letter of the previous. Prints a word snake where each word is rotated ninety degrees from the previous.
-//Can currently move in all four directions, however can run into situations where it cannot place more words
 namespace Easy_Challenge_221
 {
     class Program
     {
+        static Random random;
+
         static void Main(string[] args)
         {
+            random = new Random();
+
             Console.WriteLine("Enter the words to snake in all caps seperated by spaces." +
                 "\nEach subsequent word needs to start with the same letter as the previous word.");
             string[] words = Console.ReadLine().Split(' ');
 
             //Create a map to set to the words in, and determine the necessary width and height of the map
             char[,] letterMap;
-            int maxWordLength = 0;
-            int minWordLength = 100;
             int totalWordLength = 0;
             foreach (string word in words)
             {
-                if (word.Length > maxWordLength)
-                    maxWordLength = word.Length;
-
-                if (word.Length < minWordLength)
-                    minWordLength = word.Length;
-
                 totalWordLength += word.Length;
             }
             letterMap = new char[totalWordLength, totalWordLength];
@@ -44,146 +39,93 @@ namespace Easy_Challenge_221
                 }
             }
 
-            //Insert the words into the map, starting at the top-left corner
-            List<Direction> validDirections = new List<Direction>();
-            validDirections.Add(Direction.Down);
-            validDirections.Add(Direction.Right);
-            Random rand = new Random();
-            int[] currentPosition = new int[2] { 0, 0 };
-            for (int i = 0; i < words.Length; i++)
+            if(random.Next(0, 2) == 0)
             {
-                //Determine the direction to move along and ensure the direction can be traversed
-                int direction = rand.Next(validDirections.Count);
+                //Start with right
+                MakeWordSnake(words, 0, 1, 0, 0, 0, letterMap);
+            }
+            else
+            {
+                //Start with down
+                MakeWordSnake(words, 0, 0, 1, 0, 0, letterMap);
+            }
+            Console.ReadLine();
+        }
+        
+        
 
-                try
+        //recurse down through options, and return a boolean false if the option does not work. Assumes the map is large enough to hold all the words in at least one form
+        static bool MakeWordSnake(string[] words, int currentWord, int stepX, int stepY, int currentX, int currentY, char[,] map)
+        {
+            //loop through words[current word] adding each char to map along the specified direction
+            //if it runs into another word or runs out of room, returns false
+            map[currentX, currentY] = words[currentWord][0];
+
+            //See if the word fits
+            if(currentX + stepX * words[currentWord].Length < map.GetLength(0) && currentX + stepX * words[currentWord].Length >= 0
+                && currentY + stepY * words[currentWord].Length < map.GetLength(1) && currentY + stepY * words[currentWord].Length >= 0)
+            {
+                //Ensure the word will not intersect other words
+                for(int i = 1; i < words[currentWord].Length; i++)
                 {
-                    Direction d = validDirections[direction];
+                    if (map[currentX + stepX*i, currentY + stepY*i] != ' ')
+                        return false;
                 }
-                catch(Exception e)
+                
+                //Add the word in
+                for (int i = 1; i < words[currentWord].Length; i++)
                 {
-                    break;
+                    currentX += stepX;
+                    currentY += stepY;
+                    map[currentX, currentY] = words[currentWord][i];
+                }
+            }
+            else
+            {
+                //The word does not fit the map
+                return false;
+            }
+
+            //if there are more words to add
+            if (currentWord < words.Length - 1)
+            {
+                //after looping through, randomly shuffle a set of directions and try each in the new random order. If one succeeds (all the way down) return true
+
+                //Change the direction by ninety degrees (swap the steps)
+                int temp = stepX;
+                stepX = stepY;
+                stepY = temp;
+
+                if(random.Next(0, 2) == 0)
+                {
+                    //Reverse the steps to go in the opposite direction
+                    stepX = -stepX;
+                    stepY = -stepY;
                 }
 
-                switch (validDirections[direction])
+                if(MakeWordSnake(words, currentWord + 1, stepX, stepY, currentX, currentY, map))
                 {
-                    case Direction.Up:
-                        for (int j = 0; j < words[i].Length; j++)
-                        {
-                            letterMap[currentPosition[0], currentPosition[1]] = words[i][j];
-                            currentPosition[1]--;
-                        }
-                        currentPosition[1]++;
-                        break;
-
-                    case Direction.Down:
-                        for (int j = 0; j < words[i].Length; j++)
-                        {
-                            letterMap[currentPosition[0], currentPosition[1]] = words[i][j];
-                            currentPosition[1]++;
-                        }
-                        currentPosition[1]--;
-                        break;
-
-                    case Direction.Right:
-                        for (int j = 0; j < words[i].Length; j++)
-                        {
-                            letterMap[currentPosition[0], currentPosition[1]] = words[i][j];
-                            currentPosition[0]++;
-                        }
-                        currentPosition[0]--;
-                        break;
-
-                    case Direction.Left:
-                        for (int j = 0; j < words[i].Length; j++)
-                        {
-                            letterMap[currentPosition[0], currentPosition[1]] = words[i][j];
-                            currentPosition[0]--;
-                        }
-                        currentPosition[0]++;
-                        break;
-                }
-
-                //Determine the valid directions for the next move
-                Direction previousDirection = validDirections[direction];
-
-                validDirections.Clear();
-
-                if (previousDirection == Direction.Up || previousDirection == Direction.Down)
-                {
-                    validDirections.Add(Direction.Left);
-                    validDirections.Add(Direction.Right);
+                    return true;
                 }
                 else
                 {
-                    validDirections.Add(Direction.Up);
-                    validDirections.Add(Direction.Down);
-                }
-                
-                //Ensure the next word can fit in one of the valid directions
-                //Also ensure the word after can fit
-                if(i+1 < words.Length)
-                {
-                    int nextWordLength = words[i + 1].Length;
-
-                    if(validDirections.Contains(Direction.Up) || validDirections.Contains(Direction.Down))
-                    {
-                        //Check up
-                        validDirections.Add(Direction.Up);
-                        for (int j = 1; j < nextWordLength; j++)
-                        {
-                            if (currentPosition[1] - j < 0 || letterMap[currentPosition[0], currentPosition[1] - j] != ' ')
-                                validDirections.Remove(Direction.Up);
-                        }
-
-                        //Check down
-                        validDirections.Add(Direction.Down);
-                        for (int j = 1; j < nextWordLength; j++)
-                        {
-                            if (currentPosition[1] + j >= totalWordLength || letterMap[currentPosition[0], currentPosition[1] + j] != ' ')
-                                validDirections.Remove(Direction.Down);
-                        }
-                    }
-                    else
-                    {
-                        //Check right
-                        validDirections.Add(Direction.Right);
-                        for (int j = 1; j < nextWordLength; j++)
-                        {
-                            if (currentPosition[0] + j >= totalWordLength || letterMap[currentPosition[0] + j, currentPosition[1]] != ' ')
-                                validDirections.Remove(Direction.Right);
-                        }
-
-                        //Check left
-                        validDirections.Add(Direction.Left);
-                        for (int j = 1; j < nextWordLength; j++)
-                        {
-                            if (currentPosition[0] - j < 0 || letterMap[currentPosition[0] - j, currentPosition[1]] != ' ')
-                                validDirections.Remove(Direction.Left);
-                        }
-                    }
+                    return MakeWordSnake(words, currentWord + 1, -stepX, -stepY, currentX, currentY, map);
                 }
             }
-
-
-            for(int i = 0; i < totalWordLength; i++)
+            else
             {
-                for(int j = 0; j < totalWordLength; j++)
+                //print the word snake and return true
+                for (int i = 0; i < map.GetLength(0); i++)
                 {
-                    Console.Write(letterMap[i, j]);
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        Console.Write(map[i, j]);
+                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
+
+                return true;
             }
-
-            Console.ReadLine();
         }
-
-        enum Direction
-        {
-            Up,
-            Down,
-            Left,
-            Right
-        }
-
     }
 }
---
+++ Medium Challenges/Medium Challenge 221/Medium Challenge 221/Program.cs
@@ -0,0 +1,117 @@
+﻿//Author: Hunter Green
+//Created: 7/2/15
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+//Takes in a string array containing a word snake (see easy challenge 221). 
+//Unravels the word snake and prints the words which make up the word snake.
+//Assumes at least one word is in the word snake
+namespace Medium_Challenge_221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("challenge input2.txt");
+
+            foreach (string s in input)
+                Console.WriteLine(s);
+
+            //Move it to a character map
+            char[,] letterMap;
+
+            int maxLength = 0;
+            foreach(string s in input)
+            {
+                if (s.Length > maxLength)
+                    maxLength = s.Length;
+            }
+
+            letterMap = new char[maxLength, input.Length];
+            for(int i = 0; i < maxLength; i++)
+            {
+                for(int j = 0; j < input.Length; j++)
+                {
+                    letterMap[i, j] = ' ';
+                }
+            }
+
+            for(int y = 0; y < input.Length; y++)
+            {
+                for(int x = 0; x < input[y].Length; x++)
+                {
+                    letterMap[x, y] = input[y][x];
+                }
+            }
+
+            string words = "";
+
+            int currentX = 0;
+            int currentY = 0;
+            int stepX = 0;
+            int stepY = 0;
+
+            //Determine the starting direction
+            if(letterMap[1, 0] != ' ') { //The start direction is to the right
+                stepX = 1;
+                stepY = 0;
+            }
+            else { //The start direction is down
+                stepX = 0;
+                stepY = 1;
+            }
+
+            bool moreWords = true;
+
+            do {
+                //Loop through the word
+                while (InBounds(currentX, currentY, letterMap.GetLength(0), letterMap.GetLength(1))
+                    && letterMap[currentX, currentY] != ' ') {
+                    words += letterMap[currentX, currentY];
+                    currentX += stepX;
+                    currentY += stepY;
+                }
+
+                words += " "; //Keep a space between each word so the output is readable
+
+                //Return to the last letter of the word added
+                currentX -= stepX;
+                currentY -= stepY;
+
+                //Swap the steps for the ninety degree angle change
+                int tempStep = stepX;
+                stepX = stepY;
+                stepY = tempStep;
+
+                if (InBounds(currentX + stepX, currentY + stepY, letterMap.GetLength(0), letterMap.GetLength(1))
+                    && letterMap[currentX + stepX, currentY + stepY] != ' ') {
+                    //No change, the direction is correct
+                }
+                else if (InBounds(currentX - stepX, currentY - stepY, letterMap.GetLength(0), letterMap.GetLength(1))
+                    && letterMap[currentX - stepX, currentY - stepY] != ' ') {
+                    //The direction is opposite of the current steps, reverse them
+                    stepX = -stepX;
+                    stepY = -stepY;
+                }
+                else
+                {
+                    //If there is no next word, exit the loop
+                    moreWords = false;
+                }
+            } while (moreWords);
+
+            Console.WriteLine(words);
+
+            Console.ReadLine();
+        }
+
+        static bool InBounds(int posX, int posY, int maxX, int maxY)
+        {
+            return (posX >= 0 && posX < maxX && posY >= 0 && posY < maxY);
+        }
+    }
+}
